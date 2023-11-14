6779c4a946f516ed5e99946999a53762b6eaf5f3
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2023-11-14 23:39:19 +0300

    Update BMI calculator.cpp

--- ETS1223_15_selamawit_tefera/BMI calculator.cpp
+++ ETS1223_15_selamawit_tefera/BMI calculator.cpp
@@ -1,1 +1,17 @@
-a
+// Print Body Mass Index(BMI)
+#include<iostream>
+using namespace std;
+
+int main(){
+    float user_weight,
+		       user_height,
+		       user_BMI;
+    cout<<"please enter weight ";
+    cin>>user_weight;
+    cout<<"please enter height ";
+    cin>>user_height;
+    user_BMI=user_weight/(user_height*user_height);
+    cout<<"Body Mass Index(BMI) of user is ";
+    cout<<user_BMI;
+	return 0;
+}
