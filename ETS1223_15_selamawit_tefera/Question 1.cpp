4851d6cf5b11932acfa9020d2cdf00c8025d9f1a
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2023-11-14 22:41:21 +0300

    Update Question 1.cpp

--- ETS1223_15_selamawit_tefera/Question 1.cpp
+++ ETS1223_15_selamawit_tefera/Question 1.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int main(){
-float user_weight,
-		   user_height,
-		   user_BMI;
-cout<<"please enter weight ";
-cin>>user_weight;
-cout<<"please enter height ";
-cin>>user_height;
-user_BMI=user_weight/(user_height*user_height);
-cout<<"BMI of user is ";
-cout<<user_BMI;
+    float user_weight,
+		       user_height,
+		       user_BMI;
+    cout<<"please enter weight ";
+    cin>>user_weight;
+    cout<<"please enter height ";
+    cin>>user_height;
+    user_BMI=user_weight/(user_height*user_height);
+    cout<<"BMI of user is ";
+    cout<<user_BMI;
 	return 0;
 }
