fce7d1d983117e91db8177e4a4997e92f76e17b9
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2023-11-14 22:45:33 +0300

    Update Question 1.cpp

--- ETS1278_15_SUAD_ABRAR/Question 1.cpp
+++ ETS1278_15_SUAD_ABRAR/Question 1.cpp
@@ -1,1 +1,16 @@
- 
+//Print Size of Fundamental Types
+#include <iostream>
+using namespace std;
+
+int main(){
+float weight,
+height,
+BMI;
+cout<<"enter the weight";
+cin>>weight;
+cout<<"enter the height";
+cin>>height;
+BMI=weight/(height*height);
+cout<<"Body mass index(BMI) of a person is "<<BMI <<endl;
+return 0;
+} 
