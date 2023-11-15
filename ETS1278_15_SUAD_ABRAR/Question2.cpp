b483734d41ebf5070669a693b18eead8c4dc0281
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2023-11-15 16:32:51 +0300

    Update Question2.cpp

--- ETS1278_15_SUAD_ABRAR/Question2.cpp
+++ ETS1278_15_SUAD_ABRAR/Question2.cpp
@@ -6,8 +6,8 @@ int main(){
 float mineage, capacity;
 cout<<"please enter the mineage";
 cin>>mineage;
-cout<<"please enter the capacity";
-cin>>capacity;
-cout<<" is "<<weight/(height*height)<<endl;
+cout<<"please enter the capacity(in gallons)";
+cin>>capacity(in gallons);
+cout<<"The miles the automobile can be driven without refueling is "<<<<endl;
 return 0;
 } 
