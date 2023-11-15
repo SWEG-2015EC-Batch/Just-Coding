57c164e29d778fbde356426685d145759b5ece57
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2023-11-15 16:27:31 +0300

    Update Question2.cpp

--- ETS1278_15_SUAD_ABRAR/Question2.cpp
+++ ETS1278_15_SUAD_ABRAR/Question2.cpp
@@ -1,1 +1,13 @@
+//Print BMI
+#include <iostream>
+using namespace std;
 
+int main(){
+float mineage, capacity;
+cout<<"please enter the mineage";
+cin>>mineage;
+cout<<"please enter the capacity";
+cin>>capacity;
+cout<<" is "<<weight/(height*height)<<endl;
+return 0;
+} 
