9283ee59fff0002bb28ab52845ee9b390a8b8f66
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2023-11-15 09:20:25 +0300

    Update letter case converter.cpp

--- ETS1223_15_selamawit_tefera/letter case converter.cpp
+++ ETS1223_15_selamawit_tefera/letter case converter.cpp
@@ -7,11 +7,12 @@ int main() {
     cout << "Enter a letter: ";
     cin >> letter;
     if (islower(letter)) {
-        cout << "Uppercase equivalent: " << (char)toupper(letter) << endl;
+    cout << "Uppercase equivalent: " << (char)toupper(letter) << endl;
     } else if (isupper(letter)) {
-        cout << "Lowercase equivalent: " << (char)tolower(letter) << endl;
+     cout << "Lowercase equivalent: " << (char)tolower(letter) << endl;
     } else {
-        cout << "Invalid input. Please enter a letter." << endl;
+     cout << "Invalid input. Please enter a letter." << endl;
     }
+
     return 0;
-}l
+}
