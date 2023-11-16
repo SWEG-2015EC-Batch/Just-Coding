2336309a7291b791f54aa6429ba0348bbb5cd427
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2023-11-16 16:19:19 +0300

    Update Question3.cpp

--- ETS1278_15_SUAD_ABRAR/Question3.cpp
+++ ETS1278_15_SUAD_ABRAR/Question3.cpp
@@ -1,1 +1,21 @@
+//case converter 
+#include <iostream>
+#include <ctype.h>
+using namespace std;
 
+int main(){
+char letter;
+cout<<"please enter a letter";
+cin>>letter;
+ if (islower(letter)) {
+       char upperCaseLetter = toupper(letter);
+        cout << "Upper case equivalent " << upperCaseLetter <<endl;
+    } else if (isupper(letter)) {
+        char lowerCaseLetter = tolower(letter);
+        cout << "Lower case equivalent " << lowerCaseLetter <<endl;
+    } else {
+        cout << "Invalid input please enter a letter" <<endl;
+        }
+
+return 0;
+}
