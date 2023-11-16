2da6d1aff9a7c979b8513950d3b9ca36fcc1a12d
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2023-11-16 17:06:36 +0300

    Update ETS1278_15_SUAD_ABRAR

---
+++ ETS1278_15_SUAD_ABRAR/Question4.cpp
@@ -0,0 +1,13 @@
+//Print x^y
+#include <iostream>
+using namespace std;
+
+int main(){
+int  x, y;
+cout<<"enter x";
+cin>>x;
+cout<<"enter y";
+cin>>y;
+cout<<"Answer= "<<x<<"^"<<y <<endl;
+return 0;
+}
