00120ad816a8e9b9757f78edea4fabf1b7d5356e
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2023-11-17 23:08:11 +0300

    Update mile calculator.cpp

--- ETS1223_15_selamawit_tefera/mile calculator.cpp
+++ ETS1223_15_selamawit_tefera/mile calculator.cpp
@@ -1,15 +1,15 @@
-// program to calculate the number of miles the automobile can be driven without refueling
+// program to calculate the number of miles a automobile can be driven without refueling
 #include <iostream>
 using namespace std;
 
 int main(){
     double capacity,
-    miles_pergallom;
+    miles_pergallon;
     cout << "please enter the capacity of the fuel tank in gallons: " << endl;
     cin >> capacity;
     cout << "please enter the miles per gallon ratio of the automobile: " << endl;
-    cin >> miles_pergallom;
-    cout << "the automobile  can be driven " << (capacity * miles_pergallom) << "miles without refueling." << endl;
+    cin >> miles_pergallon;
+    cout << "the automobile  can be driven " << (capacity * miles_pergallon) << "miles without refueling." << endl;
 
     return 0;
 }
