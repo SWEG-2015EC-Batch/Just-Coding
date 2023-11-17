abb43f19f99b941143abcb60fa1e889a2b3ffd37
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2023-11-17 15:21:10 +0300

    Update Question5.cpp

--- ETS1278_15_SUAD_ABRAR/Question5.cpp
+++ ETS1278_15_SUAD_ABRAR/Question5.cpp
@@ -9,19 +9,15 @@ float workingHours, bonusRatePerHour, baseSalary;
     
     cout << "Please enter working hours: ";
     cin >> workingHours;
-    
     cout << "Please enter bonus rate per hour: ";
     cin >> bonusRatePerHour;
-    
     cout << "Please enter base salary: ";
     cin >> baseSalary;
-    
     float grossSalary = (workingHours * bonusRatePerHour) + baseSalary;
     float bonusPayment = grossSalary - baseSalary;
     float pension = grossSalary * pensionRate;
     float tax = grossSalary * taxRate;
     float netSalary = grossSalary - pension - tax + bonusPayment;
-    
     cout << "Gross Salary: " << grossSalary<<"$"<< endl;
     cout << "Bonus Payment: " << bonusPayment<<"$"<< endl;
     cout << "Net Salary: " << netSalary<<"$"<< endl;
