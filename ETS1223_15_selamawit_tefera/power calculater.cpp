5fbcbecca0d3e75b3138f89654e094ba4a02516f
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2023-11-18 00:02:31 +0300

    Update power calculater.cpp

--- ETS1223_15_selamawit_tefera/power calculater.cpp
+++ ETS1223_15_selamawit_tefera/power calculater.cpp
@@ -1,1 +1,21 @@
-a
+
+//This program calculates power of a number the user inputing the base and exponent
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    double x,
+           y,
+         result;
+    cout << "Enter x= ";
+    cin >> x;
+    cout<< "Enter y= ";
+    cin>> y; 
+    
+    result = pow(x, y);
+    
+     cout <<  x << "^" << "=" << result << endl;
+    
+
+    return 0;
+}
