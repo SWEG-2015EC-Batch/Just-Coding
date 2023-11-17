47c7871e993ca9abbe9947e4c2e157d72cb2d480
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2023-11-17 20:54:46 +0300

    Update Question6.cpp

--- ETS1278_15_SUAD_ABRAR/Question6.cpp
+++ ETS1278_15_SUAD_ABRAR/Question6.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 int main()
 { 
-int fileSize=400*1024*1024; 
-int transmissionRate=960; 
-int transmissionTime= (fileSize/transmissionRate);
+int fileSize = 400*(1024*1024); 
+int transmissionRate = 960; 
+int transmissionTime = (fileSize/transmissionRate);
 cout << "The file size is " << fileSize << " bytes" << endl;
 cout<<"The transmission line will take " << transmissionTime << " seconds to send a file." <<endl;
 return 0;
