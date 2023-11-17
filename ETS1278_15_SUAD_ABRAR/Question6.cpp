359852dd9d1b0dd1442b9c307c2608135606366c
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2023-11-17 20:50:08 +0300

    Update Question6.cpp

--- ETS1278_15_SUAD_ABRAR/Question6.cpp
+++ ETS1278_15_SUAD_ABRAR/Question6.cpp
@@ -1,11 +1,11 @@
 //Testing a program to find transmission time with given file size and transmission rate 
 //file size = 400MB in bytes
-//transmission rate is in character per second
+//transmissionrate is in character per second
 #include <iostream>
 using namespace std;
 
-int main(){ 
-
+int main()
+{ 
 int fileSize=400*1024*1024; 
 int transmissionRate=960; 
 int transmissionTime= (fileSize/transmissionRate);
