62f475e169233fe639bb93e8369700931cfa75d6
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2023-11-18 09:22:20 +0300

    Update salary calculator.cpp

--- ETS1223_15_selamawit_tefera/salary calculator.cpp
+++ ETS1223_15_selamawit_tefera/salary calculator.cpp
@@ -1,1 +1,40 @@
-a
+// This program salary details of an employee
+#include <iostream>
+#include <string>
+using namespace std;
+
+const double TAX= 0.15,
+             PENSION= 0.05;
+
+int main() 
+{
+  string name;
+  int weeklyHours;
+  double bonusRate, baseSalary, grossSalary, netSalary, bonus, tax, pension;
+
+  cout << "Enter employee name: ";
+  cin >> name;
+
+  cout << "Enter weekly working hours: ";
+  cin >> weeklyHours;
+
+  cout << "Enter bonus rate per hour: ";
+  cin >> bonusRate;
+
+  cout << "Enter base salary: ";
+  cin >> baseSalary;
+
+  bonus = weeklyHours * bonusRate;
+  grossSalary = baseSalary + bonus;
+  tax = TAX * grossSalary;
+  pension = PENSION * grossSalary;
+  netSalary = grossSalary - tax - pension;
+
+  cout << "Employee: " << name << endl;
+  cout << "Gross Salary: " << grossSalary << endl; 
+  cout << "Net Salary: " << netSalary << endl;
+  cout << "Bonus Payment: " << bonus << endl;
+
+  return 0;
+
+}
