aa56fedf154f965eeb967925d49d85ae2da2c9f8
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2023-11-18 09:43:32 +0300

    Update mile calculator.cpp

--- ETS1223_15_selamawit_tefera/mile calculator.cpp
+++ ETS1223_15_selamawit_tefera/mile calculator.cpp
@@ -7,7 +7,7 @@ int main(){
            mileage;
     cout << "please enter the capacity of the fuel tank in gallons: " << endl;
     cin >> capacity;
-    cout << "please enter the miles per gallon ratio of the automobile: " << endl;
+    cout << "please enter the mileage of the automobile: " << endl;
     cin >> mileage;
     cout << "the automobile  can be driven " << (capacity * mileage) << " miles without refueling." << endl;
 
