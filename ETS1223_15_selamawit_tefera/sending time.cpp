26269e5b9d4c0cb756f3ecc852a02f858a6d3707
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2023-11-18 09:32:39 +0300

    Update sending time.cpp

--- ETS1223_15_selamawit_tefera/sending time.cpp
+++ ETS1223_15_selamawit_tefera/sending time.cpp
@@ -1,1 +1,21 @@
-a
+// This program calculates a file sending time based on file size input 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  double fileSize, transmissionRate {960}, transmissionTime;
+
+  cout << "Enter file size in MB: ";
+  cin >> fileSize;
+
+  // Convert files' size to bytes
+  fileSize = fileSize * 1024 * 1024;
+  transmissionTime = fileSize / transmissionRate;
+
+  cout << "The time to transmit a " << fileSize << " byte file at "  
+       << transmissionRate << " characters per second is " 
+       << transmissionTime << " seconds" << endl;
+
+  return 0;
+}
