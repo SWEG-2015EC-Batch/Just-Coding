00aa5ae6f7db72a24ab55ddb74b24c7f39fbaae5
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2023-12-18 19:54:34 +0300

    Update Question 5a.cpp

--- ETS1278_15_SUAD_ABRAR/Activity 3.1/Question 5a.cpp
+++ ETS1278_15_SUAD_ABRAR/Activity 3.1/Question 5a.cpp
@@ -1,19 +1,1 @@
-#include <iostream>
-using namespace std;
 
-int main()
-{
- int temp,
- num,
- reverse;
-cout<<"enter number";
-cin>>num;
-reverse = 0;
-while (num > 0) {
-temp = num % 10;
-    reverse = (reverse * 10) + temp;
-    num = num / 10;
-}
-cout<<"reversed number= "<<reverse;
-return 0;
-} 
