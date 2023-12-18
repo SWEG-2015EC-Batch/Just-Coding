c6db025acae7a312c583d27822cf9e9b36468580
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2023-12-18 18:42:41 +0300

    Update Question 5b.cpp

--- ETS1278_15_SUAD_ABRAR/Activity 3.1/Question 5b.cpp
+++ ETS1278_15_SUAD_ABRAR/Activity 3.1/Question 5b.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+ int temp,
+ num,
+ count;
+cout<<"enter number";
+cin>>num;
+count= 0;
+while (num > 0) {
+temp = num % 10;
+   num = num / 10;
+    count++;
+}
+cout<<"number of digit = "<<count<<endl;
+return 0;
+}
