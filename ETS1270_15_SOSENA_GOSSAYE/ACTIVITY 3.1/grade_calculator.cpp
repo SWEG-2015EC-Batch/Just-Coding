d544cb51459c7dead3c55d7bf5a2b0c278032d27
sosena2 <149303452+sosena2@users.noreply.github.com>
2023-12-18 22:21:04 +0300

    Update grade_calculator.cpp

--- ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 3.1/grade_calculator.cpp
+++ ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 3.1/grade_calculator.cpp
@@ -1,1 +1,77 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    float Test, Quiz, Project, Assignment, Final_Exam,Total_mark,Mark;
+
+    cout<<"Enter your Test mark(15%): ";
+    cin>>Test;
+    if(Test<0 || Test>15){
+        cout<<"Invalid mark"<<endl<<"  Please enter a valid mark: ";
+        cin>>Test; 
+    }
+
+    cout<<"Enter your Quiz mark(5%): ";
+    cin>>Quiz;
+    if(Quiz<0 || Quiz>5){
+        cout<<"Invalid mark"<<endl<<"  Please enter a valid mark: ";
+        cin>>Quiz; 
+    }
+
+    cout<<"Enter your Project mark(20%): ";
+    cin>>Project;
+    if(Project<0 || Project>20){
+        cout<<"Invalid mark"<<endl<<"  Please enter a valid mark: ";
+        cin>>Project; 
+    }
+
+    cout<<"Enter your Assignment mark(10%): ";
+    cin>>Assignment;
+    if(Assignment<0 || Assignment>10){
+        cout<<"Invalid mark"<<endl<<"  Please enter a valid mark: ";
+        cin>>Assignment;
+    }
+
+    cout<<"Enter your Final Exam mark(50%): ";
+    cin>>Final_Exam;
+    if(Final_Exam<0 || Final_Exam>50){
+        cout<<"Invalid mark"<<endl<<"  Please enter a valid mark: ";
+        cin>>Final_Exam;
+    }
+
+    Total_mark=Test+Quiz+Project+Assignment+Final_Exam;
+    cout<<"Your Total Mark is(100%): "<<Total_mark<<endl;
+
+    if (Total_mark>=90){
+        cout<<"Your corresponding grade is: A+";
+        }else if(Total_mark>=80){
+            cout<<"Your corresponding grade is: A";
+        }else if(Total_mark>=75){
+            cout<<"Your corresponding grade is: B+";
+        }else if(Total_mark>=60){
+            cout<<"Your corresponding grade is: B";
+        }else if(Total_mark>=55){
+            cout<<"Your corresponding grade is: C+";
+        }else if(Total_mark>=45){
+            cout<<"Your corresponding grade is: C";
+        }else if(Total_mark>=30){
+            cout<<"Your corresponding grade is: D";
+        }else{
+            cout<<"Your corresponding grade is: F";
+        } 
+        
+    return 0;
+}
+    
+    
+    
+    /*switch (Total_mark){
+        case (>=90):
+        cout<<"A+";
+        break; 
+        
+        default:
+cout<<"Incvalid";
+    }
+    */
+ 
