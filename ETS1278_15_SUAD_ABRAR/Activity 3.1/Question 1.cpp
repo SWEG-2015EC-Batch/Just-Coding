9e62d42754b50d776b1bf697d8911fa8ccbeefb1
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2023-12-18 18:25:13 +0300

    Update Question 1.cpp

--- ETS1278_15_SUAD_ABRAR/Activity 3.1/Question 1.cpp
+++ ETS1278_15_SUAD_ABRAR/Activity 3.1/Question 1.cpp
@@ -1,1 +1,34 @@
- 
+#include<iostream>
+using namespace std;
+int main()
+{
+    float gross salary,overtime bonus rate,pension,income tax,bonus payment,overtime payment,net salary;
+    int worked hrs;
+    float pension rate=0.07;
+    cout<<"enter gross salary"<<gross salary<<endl;
+    cout<<"enter worked hrs"<<worked hrs<<endl;
+    cout<<"enter overtime bonus rate"<<overtime bonus rate<<endl;
+    pension=gross salary*pension rate;
+    overtime payment=(worked hrs-40)*overtime bonus rate;
+    if(gross salary<=200){
+        income tax=0;
+    }
+    else if(gross salary>200&&gross salary<=600){
+        income tax=gross salary*0.1;
+    }
+    else if(gross salary>600&&gross salary<=1200){
+        income tax=gross salary*0.15;
+    }
+    else if(gross salary>1200&&gross salary<=2000){
+        income tax=gross salary*0.2;
+    }
+    else if(gross salary>2000&&gross salary<3500){
+        income tax=gross salary*0.25;
+    }
+    else{
+        income tax=gross salary*0.3;
+    }
+    net salary=(gross salary-pension-income tax)+overtime payment;
+    cout<<"net salary= "<<net salary<<endl;
+    return 0;
+} 
