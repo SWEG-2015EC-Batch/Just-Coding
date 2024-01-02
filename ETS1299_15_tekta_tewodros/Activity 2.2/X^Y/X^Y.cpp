f78c53937bef097f26f3bf73f56b30cd14b892a6
TektaTewodros0497 <149379332+TektaTewodros0497@users.noreply.github.com>
2024-01-02 16:52:23 +0300

    Update X^Y.cpp

--- ETS1299_15_tekta_tewodros/Activity 2.2/X^Y/X^Y.cpp
+++ ETS1299_15_tekta_tewodros/Activity 2.2/X^Y/X^Y.cpp
@@ -1,1 +1,13 @@
-t
+//A program to compute the expression x^y
+#include <iostream>
+using namespace std;
+
+int main(){
+int  x, y;
+cout<<"enter x";
+cin>>x;
+cout<<"enter y";
+cin>>y;
+cout<<"Answer= "<<x<<"^"<<y <<endl;
+return 0;
+}
