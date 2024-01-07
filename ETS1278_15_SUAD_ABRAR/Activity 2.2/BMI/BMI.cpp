b5555dd9a1efc11b74495815c29654a0a8e8d299
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2024-01-07 15:54:13 +0300

    Update BMI.cpp

--- ETS1278_15_SUAD_ABRAR/Activity 2.2/BMI/BMI.cpp
+++ ETS1278_15_SUAD_ABRAR/Activity 2.2/BMI/BMI.cpp
@@ -1,1 +1,13 @@
- 
+ //BMI calculator 
+#include <iostream>
+using namespace std;
+
+int main(){
+float weight, height, BMI;
+cout<<"enter the weight";
+cin>>weight;
+cout<<"enter the height";
+cin>>height;
+cout<<"Body mass index(BMI) of a person is "<<weight/(height*height)<<endl;
+return 0;
+}  
