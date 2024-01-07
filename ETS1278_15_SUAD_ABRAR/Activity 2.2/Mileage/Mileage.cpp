0e9ab5ade06defb767d9af5a1be9aa68cbb14c90
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2024-01-07 15:59:07 +0300

    Update Mileage.cpp

--- ETS1278_15_SUAD_ABRAR/Activity 2.2/Mileage/Mileage.cpp
+++ ETS1278_15_SUAD_ABRAR/Activity 2.2/Mileage/Mileage.cpp
@@ -1,1 +1,14 @@
+//Mile calculator
+#include <iostream>
+using namespace std;
+
+int main(){
+float mileage, capacity;
+cout<<"please enter the mileage";
+cin>>mileage;
+cout<<"please enter the capacity of the fuel tank (in gallons)";
+cin>>capacity;
+cout<<"The miles the automobile can be driven without refueling is "<<mileage*capacity<<endl;
+return 0;
+} 
  
