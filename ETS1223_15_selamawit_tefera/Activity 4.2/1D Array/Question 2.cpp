e918dd8d3680fcfc8c51298b1b20f6f44b1853fa
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-13 20:36:11 +0300

    Update Question 2.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/1D Array/Question 2.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/1D Array/Question 2.cpp
@@ -1,1 +1,28 @@
+//A Program to display number taken from user and their average
+#include<iostream>
+using namespace std;
 
+ int temp [8];
+     int number;
+     float average;
+     float total=0;
+
+     for(int i=0; i<8; i++){
+      cout<<"Enter number"<<i+1<<": ";
+      cin>>number;
+      temp[i]=number;
+      total+=temp[i];
+     }
+
+     average=total/8;
+
+  cout<<"The numbers are: ";
+     for(int i=0; i<8; i++){
+            cout<<temp[i]<<" ";
+     }
+     cout<<endl;
+
+  cout<<"Average: "<<average<<endl;
+
+  return 0;
+  }
