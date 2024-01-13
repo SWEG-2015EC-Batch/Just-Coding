dd264c5d4e46092a1d2cf51bf3951513bd1fa414
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-14 01:15:40 +0300

    Update Question 6.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 6.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 6.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
 
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    int freq[256] = {0};
+
+    for (int i = 0; i < str.length(); i++)
+    {
+        freq[str[i]]++;
+    }
+
+    int largeFreq = 0;
+    char largeChar;
+
+    int smallFreq = str.length();
+    char smallChar;
+
+    for (int i = 0; i < 256; i++)
+    {
+        if (freq[i] > largeFreq)
+        {
+            largeFreq = freq[i];
+            largeChar = (char)i;
+        }
+
+        if (freq[i] > 0 && freq[i] < smallFreq)
+        {
+            smallFreq = freq[i];
+            smallChar = (char)i;
+        }
+    }
+
+    cout << "Character with large frequency is: " << largeChar << endl;
+    cout << "Character with smallest frequency is: " << smallChar << endl;
+
+    return 0;
+}
