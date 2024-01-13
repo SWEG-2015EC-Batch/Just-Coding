16753ecc0f216f34c852e1d60c3527bb5aaf7863
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-14 01:03:09 +0300

    Update Question 12.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 12.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 12.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
 
+    char answers[10] = {'D', 'C', 'B', 'B', 'A', 'C', 'D', 'A', 'D', 'B'};
+    char studentAnswers[10];
+
+    cout << "Enter your answers (A,B,C,D): ";
+
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> studentAnswers[i];
+        studentAnswers[i] = toupper(studentAnswers[i]);
+    }
+
+    int score = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        if (answers[i] ==studentAnswers[i])
+        {
+            score++;
+        }
+    }
+    cout << "Your score is: " << score << "/10" << endl;
+
+    cout << "Your answers were: ";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << studentAnswers[i] << " ,";
+    }
+
+    cout << endl;
+
+    return 0;
+}
