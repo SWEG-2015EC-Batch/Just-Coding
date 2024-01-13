59a27f7a25c3a74849d6b291e3eadce756eeb799
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-14 01:37:17 +0300

    Update Question 8.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 8.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 8.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+int main()
+{
 
+    string text;
+    cout << "Please enter a line of text: ";
+    getline(cin, text);
+
+    cout << "Text all Uppercase: ";
+    for (int i = 0; i < text.length(); ++i)
+    {
+        text[i] = toupper(text[i]);
+        cout << text[i];
+    }
+    cout <<endl<<"Text all Lowercase: ";
+    for (int i = 0; i < text.length(); ++i)
+    {
+        text[i] = tolower(text[i]);
+        cout << text[i];
+    }
+return 0;
+}
