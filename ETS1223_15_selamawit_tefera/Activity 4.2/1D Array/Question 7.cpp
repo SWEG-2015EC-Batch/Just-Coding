80bdd42258c39f0191b5b3c6d9a4cb412cda7c05
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-13 21:55:50 +0300

    Update Question 7.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/1D Array/Question 7.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/1D Array/Question 7.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  const int max = 50;
+  int nums[max] = {};
+  int n;
+  cout << "How many numbers in the list: ";
+  cin >> n;
 
+
+  cout << "Enter the numbers " << endl;
+  for (int j = 0;j < n;j++)
+    cin >> nums[j];
+
+  cout << "*********" << endl;
+  cout << "Even numbers in reverse order are :" << endl;
+
+  for (int i = n - 1;i >= 0;--i)
+  {
+    if (nums[i] % 2 == 0)
+      cout << nums[i] << endl;
+  }
+
+}
