a6a4b8afc9eec2ab857bb08633a05562f490233e
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-13 23:54:11 +0300

    Update Question 10.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 10.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 10.cpp
@@ -1,1 +1,22 @@
+//This program ask password then check if it is correct
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+int main()
+{
+  char pass[8]= "1selam1";
+  char your_pass[8];
+  cout << "Please enter your password :";
+  cin.getline(your_pass,8);
 
+  if (strcmp(pass, your_pass)==0)
+  {
+    cout << "Correcr passwrd ";
+  }
+  else
+  {
+    cout << "Incorrect password ";
+  }
+
+}
