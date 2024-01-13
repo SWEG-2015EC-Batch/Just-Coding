1578411582a19673448a009dfbce8dd6832d6f29
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-14 01:07:41 +0300

    Update Question 9.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 9.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 9.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main() {
+    char program1[] = "Young Sheldon";
+    char program2[20];
 
+    strcpy(program2, "The Big Bang Bheory");
+
+    cout << "My favorite television programs are: " << endl;
+    cout << "1, " << program1 << endl;
+    cout << "2, " << program2 << endl;
+    return 0;
+}
