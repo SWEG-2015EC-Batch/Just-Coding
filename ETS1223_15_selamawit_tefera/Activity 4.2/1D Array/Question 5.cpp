a85c242de1dff02fc60040406c49a998ca576292
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-13 22:14:08 +0300

    Update Question 5.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/1D Array/Question 5.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/1D Array/Question 5.cpp
@@ -1,12 +1,18 @@
-#include <iostream>
+//A Program to display values in three column in table form 
+#include<iostream>
 using namespace std;
-int main()
-{
-  float volts[9] = { 11.95,16.32,12.15,8.22,15.98,26.22, 13.54,6.45,17.59 };
-  for (int i = 0; i < 9; i++) {
-    cout << volts[i] << "\t\t";
-    if ((i + 1) % 3 == 0) {
-      cout << endl;
-    }
+
+  int main(){
+
+    float volt[] = {11.95, 16.32, 12.15, 8.22, 15.98, 26.22, 13.54, 6.45, 17.59};
+
+     cout<<"data entered in a three column table:"<<endl;
+       for(int i=0; i<9; i+=3){
+        for(int j=i; j<i+3; j++){
+
+        cout<<volt[j]<<"\t";
   }
-}
+    cout<<endl;
+  }
+  return 0;
+  }
