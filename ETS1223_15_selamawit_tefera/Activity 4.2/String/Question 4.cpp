b31ff5564cddf502f7294ce39f5c04febe1face2
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-13 23:47:01 +0300

    Update Question 4.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 4.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 4.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  cout << "Please enter your text: ";
+  string text;
+  getline(cin, text);
 
+  for (int i = 0;i < text.length();i++)
+  {
+    while (i < text.length() && text[i] != ' ')
+    {
+      cout << text[i]<<",";
+      i++;
+    }
+    cout <<endl;
+
+  }
+
+}
