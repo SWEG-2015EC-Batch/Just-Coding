e5e4b2c1024431696c660a595b4eab4447ea1456
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-13 23:01:26 +0300

    Update Question 4.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/2D Array/Question 4.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/2D Array/Question 4.cpp
@@ -1,1 +1,36 @@
+using namespace std;
+#include<iostream>
+int main()
+{
 
+  int num[3][4];
+  int sum = 0;
+  cout << "Enter your 12 array elements \n";
+  for (int i = 0;i < 3;i++)
+  {
+    for (int j = 0;j < 4;j++)
+    {
+      cin >> num[i][j];
+      if (num[i][j] % 2 == 0)
+      {
+        sum = sum + num[i][j];
+      }
+    }
+  }
+  cout << " -------------------------------- "<<endl;
+
+  for (int i = 0;i < 3;i++)
+  {
+    for (int j = 0;j < 4;j++)
+    {
+      if (num[i][j] % 2 == 0)
+        cout << num[i][j]<<"\t | ";
+      else
+        cout << "* "<<"\t | ";
+    }
+    cout << endl;
+  }
+  cout << " -------------------------------- " << endl;
+  cout << "The sum of given even integers is= " << sum;
+  return 0;
+}
