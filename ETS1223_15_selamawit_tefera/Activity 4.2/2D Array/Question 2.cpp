4e118fef1ca1252dc4ce5c4b3dcaba53ec65b839
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-13 22:45:09 +0300

    Update Question 2.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/2D Array/Question 2.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/2D Array/Question 2.cpp
@@ -1,1 +1,26 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+  const int row=5;
+  const int col = 5;
+  int num[row][col];
+  int product=1, sum=0;
+  cout << "List five row elements :"<<endl;
+  for (int i = 0;i < row;i++)
+  {
+    cin >> num[i][0];
+    product = product * num[i][0];
+  }
+  cout << "List five columun elements :" << endl;
+  for (int i = 0;i < col;i++)
+  {
+    cin >> num[0][i];
+    sum = sum + num[0][i];
+  }
+  cout << "Rows \t\t Columuns\t\tProduct of rows\t\t Sum of Columns"<<endl;
+  for (int i = 0;i < 5;i++)
+  {
+    cout << num[i][0]<<"\t\t  "<<num[0][i]<<"\t\t\t"<<product<< "\t\t          "<<sum<<endl;
+  }
+}
