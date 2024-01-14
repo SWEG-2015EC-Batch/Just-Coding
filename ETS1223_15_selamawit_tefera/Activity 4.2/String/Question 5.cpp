484a233a82c57119928a06950ef10fd1a76b6b71
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-14 18:34:19 +0300

    Update Question 5.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 5.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 5.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
+int main(){
 
+    char text[200];
+    int vowels = 0, consonants = 0, digits = 0, specialChar(0);
+
+    cout<<"Enter your text: ";
+    cin.getline (text,200);
+
+    for (int i = 0; text[i] != '\0' ; i++){
+
+        if(text[i] == 'a' || text[i] == 'e' || text[i] == 'i' || text[i] == 'o' || text[i] == 'u' 
+        || text[i] == 'A' || text[i] == 'E' || text[i] == 'I' || text[i] == 'O' || text[i] == 'U'){
+            vowels++;
+        }else if((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z' ) ){
+            ++consonants;
+        }else if(text[i] >= '0' && text[i] <= '9'){
+            ++digits;
+        }else{
+            ++specialChar;
+        }
+        
+    }
+
+    cout<< "The number of vowels: "<<vowels<<endl;
+    cout<< "The number of consonants: "<<consonants<<endl;
+    cout<< "The number of digits: "<<digits<<endl;
+    cout<< "The number of special Characters: "<<specialChar<<endl;
+    
+    return 0;
+}
