9763d77ceb8af72db36b7ccdef112011cc78b2bf
sosena2 <149303452+sosena2@users.noreply.github.com>
2024-01-14 13:44:41 +0300

    Update QUESTION3.5.cpp

--- ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/STRING/QUESTION3.5.cpp
+++ ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/STRING/QUESTION3.5.cpp
@@ -1,1 +1,36 @@
-
+//program that displays the frequency of vowel, consonant, digit and special character 
+	string input;
+	
+	cout<<"Enter a string"<<endl;
+	getline(cin,input);
+	
+	int vowel=0, consonants=0, digits=0, specialChars=0;
+	
+	for(int i=0; i<input.length(); i++)
+	{
+    		char ch =tolower(input[i]);
+    		
+    	if (ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u')
+		{
+    		vowel++;		
+		}
+		else if(ch>='a'&& ch<='z')
+		{
+			consonants++;
+		}
+	    else if(isdigit(ch))
+  		{
+		digits++;
+		}
+	 	else 
+		{
+		specialChars++;
+		}
+		
+	}
+cout<<"vowels: "<<vowel<<endl;
+cout<<"consonants: "<<consonants<<endl;
+cout<<"digits: "<<digits<<endl;
+cout<<"special characters: "<<specialChars<<endl;
+	return 0;
+}
