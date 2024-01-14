29ffb7a3a7ca7306225d85f01e3d3bc523995b9a
sosena2 <149303452+sosena2@users.noreply.github.com>
2024-01-14 13:10:44 +0300

    Update QUESTION1.cpp

--- ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/2D_ARRAY/QUESTION1.cpp
+++ ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/2D_ARRAY/QUESTION1.cpp
@@ -1,1 +1,20 @@
-
+//the program checks if the two arrays are identical or not.
+#include<iostream>
+using namespace std;
+int main(){
+	string comment;
+	int arr1[5]={1,2,3,4,5}, arr2[5]={1,2,3,4,5};
+	
+	for(int i=0; i<5; i++){
+			
+		if(arr1[i]==arr2[i]){
+			comment="The two arrays have same element.";
+			
+		}else{
+			comment="The two arrays have different elements.";
+		}
+			
+		}
+	cout<<comment;
+	return 0;
+}
