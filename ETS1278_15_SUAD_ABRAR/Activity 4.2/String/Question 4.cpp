d61829fb11edd75432c1df02ec2902e96b2cf041
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2024-01-14 21:53:58 +0300

    Update Question 4.cpp

--- ETS1278_15_SUAD_ABRAR/Activity 4.2/String/Question 4.cpp
+++ ETS1278_15_SUAD_ABRAR/Activity 4.2/String/Question 4.cpp
@@ -1,1 +1,23 @@
+// A Program to inputs a line of text, and tokenizes an application 
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main() {
+    char word[100];
+
+    cout << "Enter your sentence:" << endl;
+    cin.getline(word, 100);
+
+    for (int i = 0; word[i] != '\0'; i++) {
+
+        if (word[i] == ' ') {
+            cout << endl;
+        } else {
+            cout << word[i];
+        }
+    }
+
+    return 0;
+}
  
