247a657630eee96c8f1ef39d5d1d33df1c3b22ef
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2024-01-14 22:11:19 +0300

    Update Question 10.cpp

--- ETS1278_15_SUAD_ABRAR/Activity 4.2/String/Question 10.cpp
+++ ETS1278_15_SUAD_ABRAR/Activity 4.2/String/Question 10.cpp
@@ -1,1 +1,24 @@
+//A Program that validates password
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+int main()
+{
+  char pass[10]= "sos123456";
+  char your_pass[10];
+
+  cout << "Please enter your password :";
+  cin.getline(your_pass,10);
+
+  if (strcmp(pass, your_pass)==0){
+
+    cout << "you have entered the correct password ";
+  }
+  else
+  {
+    cout << "Incorrect password ";
+  }
+  return 0;
+}
  
