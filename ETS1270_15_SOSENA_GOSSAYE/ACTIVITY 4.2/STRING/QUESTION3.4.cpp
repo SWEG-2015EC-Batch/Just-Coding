0d0a81b3feead64821a3560ab36e8e3fae05d7f4
sosena2 <149303452+sosena2@users.noreply.github.com>
2024-01-14 13:40:26 +0300

    Update QUESTION3.4.cpp

--- ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/STRING/QUESTION3.4.cpp
+++ ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/STRING/QUESTION3.4.cpp
@@ -1,1 +1,22 @@
+//the program tokenizes a line of text
+#include<iostream>
+#include<string>
+#include<sstream>
+using namespace std;
+int main(){
 
+	string input;
+	
+	cout<<"please enter a line of text"<<endl;
+	getline(cin,input);
+	
+	istringstream iss(input);
+	string token;
+	
+	cout<<"tokens:"<<endl;
+	while(iss>>token)
+	{
+		cout<<token<<endl;
+    return 0;
+	}
+	
