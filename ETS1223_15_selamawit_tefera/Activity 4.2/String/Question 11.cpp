02fc01ddd93cd897475d0a2ff5b97c0a08a6af39
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-14 18:12:03 +0300

    Update Question 11.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 11.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 11.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <string.h>
 
+using namespace std;
+int main(){
+
+    char firstName[20];
+    char middleName[20];
+    char lastName[20];
+
+    cout<<"Please enter your first name: ";
+    cin>>firstName;
+    cout<<"Please enter your middle name: ";
+    cin>>middleName;
+    cout<<"Please enter your last name: ";
+    cin>>lastName;
+
+    cout<<"The initials of your fullname are: ";
+    cout<<firstName[0]<<"  "<<middleName[0]<<"  "<<lastName[0]<<endl; 
+
+
+    return 0;
+}
