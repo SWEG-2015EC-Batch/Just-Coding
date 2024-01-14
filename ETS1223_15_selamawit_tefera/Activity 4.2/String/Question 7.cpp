470cc5714adf626db277999eb6b04a34c9bc55b7
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-14 18:23:19 +0300

    Update Question 7.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 7.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 7.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
+int main(){
 
+    int num ;
+
+    cout<<"How many people you want to enter? ";
+    cin>>num;
+
+    string names[num], temp;
+
+    cout<<"Enter the names of the "<<num<<" people:" <<endl;
+    for (int i = 0; i < num; i++){
+
+        cin>>names[i];
+
+    }
+
+    for (int i = 0; i < num; ++i){
+        for (int j = i + 1; j < num; ++j){
+            if(names[i] > names[j]){
+                temp = names[i];
+                names[i] = names[j];
+                names[j] = temp;
+
+            }
+        
+        }
+
+    }
+
+    cout<<"The names alphabeticaly are: "<<endl;
+    for (int i = 0; i < num; ++i){
+
+        cout<<names[i]<<endl;
+
+    }    
+
+    return 0;
+}
