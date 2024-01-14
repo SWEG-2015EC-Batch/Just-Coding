652ae82bd00aa9a11f4400867dd76ac063a39f19
sosena2 <149303452+sosena2@users.noreply.github.com>
2024-01-14 14:02:57 +0300

    Update QUESTION3.8.cpp

--- ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/STRING/QUESTION3.8.cpp
+++ ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/STRING/QUESTION3.8.cpp
@@ -1,1 +1,25 @@
+//program that display string in upper and lower case 
+#include<iostream>
+#include<string>
 
+using namespace std;
+int main(){
+	//string in upper and lower case working question 8
+	string input;
+	
+	cout<<"Enter a string"<<endl;
+	getline(cin,input);
+	
+	cout<<"string in uppercase is:"<<endl;
+	for(int i=0; i<input.length(); i++)
+	{
+	 putchar(toupper(input[i]));
+	}
+	
+	cout<<"\nstring in lowercase is:"<<endl;
+	for(int i=0; i<input.length(); i++)
+	{
+	 putchar(tolower(input[i]));
+	}
+return 0;
+}
