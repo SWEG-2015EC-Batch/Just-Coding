984fd1f2588e37906a7586b14730134ae861be9b
TektaTewodros0497 <149379332+TektaTewodros0497@users.noreply.github.com>
2024-01-14 22:57:14 +0300

    Update Question 1.cpp

--- ETS1299_15_tekta_tewodros/Activity 4.2/2D ARRAY/Question 1.cpp
+++ ETS1299_15_tekta_tewodros/Activity 4.2/2D ARRAY/Question 1.cpp
@@ -1,1 +1,24 @@
-t
+// A Program that check whether two one dimensional arrays of same size are identical or not
+#include<iostream>
+using namespace std;
+
+ int main(){
+
+     int row1 , row2;
+
+    cout<<"Enter the number of row1: ";
+    cin >> row1;
+    cout<<"Enter the number of row2: ";
+    cin >> row2;
+
+    if(row1 == row2){
+
+        cout<<"array1 and array2 are identical";
+
+    }else{
+
+    cout<<"array1 and array2 are not identical";
+    }
+
+    return 0;
+}
