02a47160155d84ea7240b2161fc3ec228d894616
sosena2 <149303452+sosena2@users.noreply.github.com>
2024-01-14 12:08:30 +0300

    Update QUESTION1.cpp

--- ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/1D_ARRAY/QUESTION1.cpp
+++ ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/1D_ARRAY/QUESTION1.cpp
@@ -1,1 +1,17 @@
-
+//declaration and initialization of 4 by 5 array
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int num[4][5]={{16,22,99,4,18},{-258,4,101,5,98},{105,6,15,2,45},{33,88,72,16,3}};
+  cout<<"the array is displayed as follows:"<<endl;
+  
+	for(inti=0; i<4; i++)
+	{
+		for(int j=0; j<5; j++)
+		{
+			cout<<num[i][j]<<"\t";
+		}
+		cout<<endl;
+	}
+}
