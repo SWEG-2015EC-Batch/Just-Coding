61982d3d1998b3c353904f5db9514e72037429bd
TektaTewodros0497 <149379332+TektaTewodros0497@users.noreply.github.com>
2024-01-14 23:14:34 +0300

    Update Question 2.cpp

--- ETS1299_15_tekta_tewodros/Activity 4.2/STRING/Question 2.cpp
+++ ETS1299_15_tekta_tewodros/Activity 4.2/STRING/Question 2.cpp
@@ -1,1 +1,27 @@
-y
+//A Program that checks either a word is palindrome or not.
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+
+  string word;
+  cout << "Enter your word ";
+  getline(cin,word);
+  string reverse_word;
+
+  for (int i = word.length() - 1;i >= 0;i--)
+  {
+      reverse_word = reverse_word + word[i];
+  }
+  if (word == reverse_word)
+  {
+    cout << "The word is palindrome ";
+  }
+  else
+  {
+    cout << "The word is not Palindrome ";
+  }
+  return 0;
+}
