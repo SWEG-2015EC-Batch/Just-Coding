4db13429613e08196878eafd088b805c285bf85a
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2024-01-14 22:16:40 +0300

    Update Question 12.cpp

--- ETS1278_15_SUAD_ABRAR/Activity 4.2/String/Question 12.cpp
+++ ETS1278_15_SUAD_ABRAR/Activity 4.2/String/Question 12.cpp
@@ -1,1 +1,29 @@
- 
+//A Program to store an answer sheet for a test containing 10 multiple choice
+questions
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+  int main(){
+    char
+ correct[10]={'A','B','C','D','E','A','B','C','D','E'};
+    char trytest[10];
+    int counter = 0;
+
+    for(int i=0;i<10;i++){
+
+        cout<<"Answer for "<<i+1<<": ";
+        cin>>trytest[i];
+    }
+
+    for(int i=0;i<10;i++){
+
+        if (toupper(trytest[i])==correct[i]){
+            counter++;
+        }
+    }
+
+    cout<<"Your score for this test is: "<<counter;
+
+return 0;
+} 
