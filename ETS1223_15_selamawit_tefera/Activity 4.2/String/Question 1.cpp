759b6827ae604f7b2989381f93872811fb2b48b8
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-14 18:56:56 +0300

    Update Question 1.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 1.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 1.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <ctype.h>
+using namespace std;
+int main(){
 
+    char text[300];
+    int vowels = 0, consonants = 0;
+
+    cout<<"Enter a string: ";
+    cin.getline (text,300);
+
+    for (int i = 0; text[i] != '\0' ; i++){
+
+        if(text[i] == 'a' || text[i] == 'e' || text[i] == 'i' || text[i] == 'o' || text[i] == 'u' ){
+            vowels++;
+        }else if(text[i] >= 'a' && text[i] <= 'z'){
+            ++consonants;
+        }
+        
+    }
+
+    cout<< "The number of vowels in the string is: "<<vowels<<endl;
+    cout<< "The number of consonants in the string is: "<<consonants<<endl;
+    
+    return 0;
+}
