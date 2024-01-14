33f2184fed0f5d97c5429a692bafa966590ac091
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-14 18:44:04 +0300

    Update Question 3.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 3.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 3.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
 
+    string mixed,temp = "";
+
+    cout<<"Enter a string: ";
+    getline(cin,mixed);
+
+    int sizeOfmixed = mixed.size();
+
+    for (int i = 0; i < sizeOfmixed; i++){
+
+        if((mixed[i] >= 'a' && mixed[i] <= 'z') || (mixed[i] >= '0' && mixed[i] <= '9' ) ){
+
+            temp += mixed[i];
+        }
+
+    }
+
+    mixed = temp;
+
+    cout<<"The alphanumerical characters of the string are: "<<mixed<<endl;
+  
+    return 0;
+
+}
