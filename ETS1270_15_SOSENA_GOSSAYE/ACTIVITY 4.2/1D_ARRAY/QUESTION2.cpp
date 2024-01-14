8ca5fcb229e826d48f01807256ffed1918bfd9b7
sosena2 <149303452+sosena2@users.noreply.github.com>
2024-01-14 12:46:23 +0300

    Update QUESTION2.cpp

--- ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/1D_ARRAY/QUESTION2.cpp
+++ ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/1D_ARRAY/QUESTION2.cpp
@@ -1,1 +1,23 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+	int temp[8], total=0, average;
+ for(int i=0; i<8; i++)
+ {
+		cout<<"please enter number"<<i+1<<endl;
+		cin>>temp[i];
+		
+		total=total+temp[i];
+	average= total/8;
+		
+	}
+	
+	cout<<"the numbers are:"<<endl;
+	
+	for(int j=0; j<8; j++){
+	
+		cout<<temp[j]<<" ";
+	}
+	cout<<"\ntheir average is:"<<average;*/
+    return 0;
+}
