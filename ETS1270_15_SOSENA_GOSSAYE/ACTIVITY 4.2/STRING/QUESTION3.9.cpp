9e9cd4385784cfefe372c93a3b7b78f8aef42b4a
sosena2 <149303452+sosena2@users.noreply.github.com>
2024-01-14 14:06:25 +0300

    Update QUESTION3.9.cpp

--- ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/STRING/QUESTION3.9.cpp
+++ ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/STRING/QUESTION3.9.cpp
@@ -1,1 +1,15 @@
+//program that displays my favourite tv program
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main() {
+    char program1[] = "friends";
+    char program2[20];
 
+    strcpy(program2, "tech talk with solomon");
+
+    cout << "My favorite television programs are: " << endl;
+    cout << "1, " << program1 << endl;
+    cout << "2, " << program2 << endl;
+    return 0;
+}
