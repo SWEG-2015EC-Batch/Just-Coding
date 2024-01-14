a67f3685709cf1907fdb90e2f30413db2259bbfe
sosena2 <149303452+sosena2@users.noreply.github.com>
2024-01-14 13:28:55 +0300

    Update QUESTION3.1.cpp

--- ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/STRING/QUESTION3.1.cpp
+++ ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/STRING/QUESTION3.1.cpp
@@ -1,1 +1,30 @@
+//consonant and vowel counter in a string 
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	
+	string word;
+	cout<<"please enter a string"<<endl;
+	cin>>word;
+	
+    int consonantCount=0, vowelCount=0;
+    
+    for(int i=0; i<word.length(); i++)
+	{
+    		char ch =tolower(word[i]);
+    		
+    	if (ch=='a'|| ch=='e'||ch=='i'||ch=='o'||ch=='u')
+		{
+    		vowelCount++;		
+		}
+		else
+		{
+			consonantCount++;
+		}
+	}
+cout<<"the string contains "<<vowelCount<<" vowel letters"<<endl;
+cout<<"the string contains "<<consonantCount<<" consonant letters"<<endl;
 
+  return 0;
+}
