dfaee465b0c4686480f6f49f3ffe684deb930dc4
sosena2 <149303452+sosena2@users.noreply.github.com>
2024-01-14 13:36:01 +0300

    Update QUESTION3.3.cpp

--- ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/STRING/QUESTION3.3.cpp
+++ ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/STRING/QUESTION3.3.cpp
@@ -1,1 +1,21 @@
+//the program erases nonalphabet character from a string.
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
 
+  string word;
+	cout<<"please enter a string"<<endl;
+	cin>>word;
+	
+	for(int i=0; i<word.size(); i++)
+	{
+		if(word[i]<'A'||word[i]>'Z' && word[i]<'a'||word[i]>'z')
+		{
+			word.erase(i,1);
+			i--;
+		}
+	}
+	cout<<word;
+return 0;
+}
