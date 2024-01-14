0784d104a278719a6f7ccfe44ec11baf403f9810
selamawittefera <149393912+selamawittefera@users.noreply.github.com>
2024-01-14 19:16:48 +0300

    Update Question 1.cpp

--- ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 1.cpp
+++ ETS1223_15_selamawit_tefera/Activity 4.2/String/Question 1.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <ctype.h>
 using namespace std;
 int main(){
 
@@ -11,13 +10,14 @@ int main(){
 
     for (int i = 0; text[i] != '\0' ; i++){
 
-        if(text[i] == 'a' || text[i] == 'e' || text[i] == 'i' || text[i] == 'o' || text[i] == 'u' ){
+        if(text[i] == 'a' || text[i] == 'e' || text[i] == 'i' || text[i] == 'o' || text[i] == 'u'  
+		|| text[i] == 'A' || text[i] == 'E' || text[i] == 'I' || text[i] == 'O' || text[i] == 'U'){
             vowels++;
-        }else if(text[i] >= 'a' && text[i] <= 'z'){
+        }else if((text[i] >= 'a' && text[i] <= 'z')|| (text[i] >= 'A' && text[i] <= 'Z' )){
             ++consonants;
         }
-        
     }
+    
 
     cout<< "The number of vowels in the string is: "<<vowels<<endl;
     cout<< "The number of consonants in the string is: "<<consonants<<endl;
