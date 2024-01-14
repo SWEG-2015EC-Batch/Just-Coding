01be795823837f8bca9d17f3e202702c92092f65
TektaTewodros0497 <149379332+TektaTewodros0497@users.noreply.github.com>
2024-01-14 23:27:37 +0300

    Update Question 9.cpp

--- ETS1299_15_tekta_tewodros/Activity 4.2/STRING/Question 9.cpp
+++ ETS1299_15_tekta_tewodros/Activity 4.2/STRING/Question 9.cpp
@@ -1,1 +1,17 @@
-t
+// A Program to store and print the names of two favorite television programs
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+
+    char program1[50]="Friends";
+    char program2[50];
+
+    strcpy(program2,"Breaking Bad");
+
+     cout << "First Program: " << program1 << endl;
+
+     cout << "Second Program: " << program2 << endl;
+} 
+return 0;
+}
