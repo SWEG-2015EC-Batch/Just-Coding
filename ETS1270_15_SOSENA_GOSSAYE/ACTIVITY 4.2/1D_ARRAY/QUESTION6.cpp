55cf9dd980fcf1d96bc6e6532343130e8ca0b3e9
sosena2 <149303452+sosena2@users.noreply.github.com>
2024-01-14 13:05:21 +0300

    Update QUESTION6.cpp

--- ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/1D_ARRAY/QUESTION6.cpp
+++ ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/1D_ARRAY/QUESTION6.cpp
@@ -1,1 +1,33 @@
-
+//the program displays even numbers in reverse order from list of numbers
+#include<iostream>
+using namespace std;
+int main(){
+	int evenNum[100], value, count=0;
+	
+	cout<<"Enter series of numbers(enter 0 to stop):"<<endl;
+	while(true){
+		cout<<"Enter a number: ";
+		cin>>value;
+		if(value==0){
+			break;
+		}
+		if(value%2==0){
+			evenNum[count]=value;
+			count++;
+		}
+	}
+	
+	if(count==0){
+		cout<<"No even numbers were entered."<<endl;
+	}else{
+		cout<<"Even numbers in reverse order are:"<<endl;
+		for (int i=count-1;i>=0;i--){
+			cout<<evenNum[i]<<" ";
+		}
+		
+		cout<<endl;
+	}
+	
+return 0;		
+	
+}
