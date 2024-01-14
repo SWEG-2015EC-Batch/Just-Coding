0fcefae306ffd4ef52b53d6fa31bbcc4759ed9e0
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2024-01-14 21:50:03 +0300

    Update Question 3.cpp

--- ETS1278_15_SUAD_ABRAR/Activity 4.2/String/Question 3.cpp
+++ ETS1278_15_SUAD_ABRAR/Activity 4.2/String/Question 3.cpp
@@ -1,1 +1,33 @@
+//A Program to remove non-alphabet character from string
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+
+int main()
+{
+  char word[100];
+  cout<<"Please enter your character"<<endl;
+  cin.getline(word,100);
+
+  for(int i=0;word[i]!='\0';i++){
+
+    if((word[i]>='a'&& word[i]<='z')||(word[i]>='A'&& word[i]<='Z')){
+       cout<<word[i];
+      } else if(word[i]==' '){
+        continue;
+      }
+
+       }
+    cout<<endl;
+    return 0;
+  }
+  
+  
+
+
+
+  
+
+
  
