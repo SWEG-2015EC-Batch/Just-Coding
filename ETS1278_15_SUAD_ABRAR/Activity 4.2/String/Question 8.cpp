f66e61637bad268574a05255edbde0caefc703e6
SuadAbrar <149161122+SuadAbrar@users.noreply.github.com>
2024-01-14 22:06:10 +0300

    Update Question 8.cpp

--- ETS1278_15_SUAD_ABRAR/Activity 4.2/String/Question 8.cpp
+++ ETS1278_15_SUAD_ABRAR/Activity 4.2/String/Question 8.cpp
@@ -1,1 +1,39 @@
+//A Program to input a line of text and output the text twice, once in all uppercase
+and once in all lowercase letters.
+
+#include<iostream>
+#include<cstring>
+#include<cctype>  
+using namespace std;
+
+int main() {
+    char text[100];
+
+    cout<<"Enter your text"<<endl;
+    cin.getline(text,100);
+
+    char lower[100];
+    char upper[100];
+
+    cout << "Lowercase text:" << endl;
+
+    for(int i=0;text[i]!='\0';i++){
+
+      lower[i]=tolower(text[i]);
+      cout<<lower[i];
+    }
+
+    cout<<endl;
+    cout << "Uppercase text:" << endl;
+
+    for(int i=0;text[i]!='\0';i++){
+
+      upper[i]=toupper(text[i]);
+      cout<<upper[i];
+    }
+
+    cout<<endl;
+
+    return 0;
+}
  
