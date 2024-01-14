db2fa4f5ae104f2321acc4b8c4aecadf233e4e17
sosena2 <149303452+sosena2@users.noreply.github.com>
2024-01-14 13:13:21 +0300

    Update QUESTION2.cpp

--- ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/2D_ARRAY/QUESTION2.cpp
+++ ETS1270_15_SOSENA_GOSSAYE/ACTIVITY 4.2/2D_ARRAY/QUESTION2.cpp
@@ -1,1 +1,56 @@
+//program that shows the product of row elements and sum of column elements 
+#include<iostream>
+using namespace std;
+int main(){
+int rows,cols;
+int arr[rows][cols];
+
+cout<<"enter the number of the row of matrix: "<<endl;
+cin>>rows;
+cout<<"enter the number of column of matrix"<<endl;
+cin>>cols;
+
+cout<<"enter the elements of the matrix: "<<endl;
+
+for(int i=0; i<rows; i++)
+{
+	for(int j=0; j<cols; j++)
+	{
+		cin>>arr[i][j];
+	}
+
+}	
+
+cout<<"the matrix elements are:"<<endl;
+for(int i=0; i<rows; i++)
+{
+	for(int j=0; j<rows; j++)
+	{
+		cout<<arr[i][j]<<"\t";
+	}
+	cout<<endl;
+	}	
+	
+	for(int i=0; i<rows; i++)
+	{
+		int product=1;
+       for(int j=0; j<cols; j++)
+	   {
+	
+       	product*=arr[i][j];
+	   }
+	cout<<"the product of elements of row "<<i+1<<": "<<product<<endl;
+	}
+	for(int i=0; i<cols; i++){
+		int sum=0;
+		for(int j=0; j<rows; j++)
+		{
+	    sum+=arr[j][i];		
+		
+		}
+		cout<<"the sum of elements of column "<<i+1<<": "<<sum<<endl;
+	}
+return 0;	
+}	
+	
 
