efcade972ac24d78370e38a60e6899e8632a213d
TektaTewodros0497 <149379332+TektaTewodros0497@users.noreply.github.com>
2024-01-14 23:29:50 +0300

    Update Question 11.cpp

--- ETS1299_15_tekta_tewodros/Activity 4.2/STRING/Question 11.cpp
+++ ETS1299_15_tekta_tewodros/Activity 4.2/STRING/Question 11.cpp
@@ -1,1 +1,22 @@
-c
+//A Program that displays initials of names
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+  string firstName, middleName, lastName;
+
+    cout << "Enter your first name: ";
+    cin >> firstName;
+
+    cout << "Enter your middle name: ";
+    cin >> middleName;
+
+    cout << "Enter your last name: ";
+    cin >> lastName;
+
+    cout << "Your initials are: " << firstName[0] << middleName[0] << lastName[0] << endl;
+
+    return 0;
+}
